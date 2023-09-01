6b6e9753ef6db134b0a59c07f50b6b731ab797bd
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2023-09-01 21:13:00 +1000

    add field event support

--- sports_tracker_app.py
+++ sports_tracker_app.py
@@ -148,7 +148,7 @@ def import_results(file, athletes_directory, output, year):
                     # ensure that the athlete exists
                     simple_tally.lookup_athlete(filename, athletes_directory)
 
-                    result_in_seconds = 0
+                    result_in_seconds = None
                     if re.match(r"^[0-9]+m [0-9]+s [0-9]+ms$", result):
                         result_split = result.split(" ")
                         result_in_seconds = (
@@ -170,41 +170,145 @@ def import_results(file, athletes_directory, output, year):
                             "finish_time": result_in_seconds,
                         }
                     )
-            # elif re.match(r"^#[0-9]+ Long jump Year ([7-9]|10) (fe)?male$", event_name):
-            #     event_name_split = event_name.split(" ")
-            #     event_number = event_name_split[0][1:]
-            #     event_year = event_name_split[4]
-            #     event_gender = event_name_split[5]
-            #     date_start = datetime.datetime(2023, 9, 2, 9, 0, 0)
-            #     date_start += datetime.timedelta(minutes=(int(event_number) - 1) * 10)
-            #     event_json = {
-            #         "type": "long_jump",
-            #         "name": f"Long Jump Year {event_year} {event_gender}",
-            #         "distance": int(event_distance),
-            #         "gender": event_gender,
-            #         "ystart": year - int(event_year),
-            #         "date": date_start.isoformat(),
-            #         "results": [],
-            #     }
+            elif re.match(
+                r"^#[0-9]+ (Long jump|Javelin throw|Triple jump|Shot put|Discus throw) Year ([7-9]|10) (fe)?male$",
+                event_name,
+            ):
+                event_name_split = event_name.split(" ")
+                if len(event_name_split) == 6:
+                    event_number = event_name_split[0][1:]
+                    event_year = event_name_split[4]
+                    event_gender = event_name_split[5]
+                    event_type = event_name_split[1:3]
+                else:
+                    continue
+                date_start = datetime.datetime(2023, 9, 2, 9, 0, 0)
+                date_start += datetime.timedelta(minutes=(int(event_number) - 1) * 10)
+                event_json = {
+                    "type": "_".join(event_type).lower(),
+                    "name": " ".join(event_type).title()
+                    + f" Year {event_year} {event_gender}",
+                    "gender": event_gender,
+                    "ystart": year - int(event_year),
+                    "date": date_start.isoformat(),
+                    "results": [],
+                }
 
-            #     # iterate through rows B7:F(INFINITY)
-            #     for row in range(6, sheet.shape[0]):
-            #         # C: Name, E: Result
-            #         # ***REMOVED***, 14s 600ms
-            #         name = sheet.iloc[row, 2]
-            #         name_split = name.split(" ")
-            #         result = sheet.iloc[row, 4]
-            #         filename = (
-            #             "-".join(name_split[0:2]).lower()
-            #             + "-"
-            #             + str(event_json["ystart"])
-            #             # + "*"
-            #         )
-            #         athlete = simple_tally.lookup_athlete(filename, athletes_directory)
-            else:
-                print(
-                    f"Unknown event type with {sheet.shape[0]-5} results: {event_name}"
-                )
+                # iterate through rows B7:F(INFINITY)
+                for row in range(6, sheet.shape[0]):
+                    name = sheet.iloc[row, 2]
+                    name_split = name.split(" ")
+                    result = sheet.iloc[row, 4]
+                    filename = (
+                        "-".join(name_split[0:2]).lower()
+                        + "-"
+                        + str(event_json["ystart"])
+                        # + "*"
+                    )
+                    athlete = simple_tally.lookup_athlete(filename, athletes_directory)
+
+                    # 1 4m 60cm2 metersm centimeterscm3 metersm centimeterscm
+                    # 1: 4600
+                    # 1 4m 30cm2 4m 12cm3 metersm centimeterscm
+                    # 1: 4300, 2: 4120
+                    # 1 4m 22cm2 metersm centimeterscm3 metersm centimeterscm
+                    # 1: 4220
+
+                    current_result = None
+
+                    if re.match(
+                        r"^1 ([0-9]*|meters)m ([0-9]*|centimeters)cm2 ([0-9]*|meters)m ([0-9]*|centimeters)cm3 ([0-9]*|meters)m ([0-9]*|centimeters)cm$",
+                        result,
+                    ):
+                        current_result = []
+                        results_split = result.split("cm")
+                        # ['1 4m 30', '2 4m 12', '3 metersm centimeters', '']
+                        for result_split in results_split:
+                            if not result_split:
+                                continue
+                            result_split_split = result_split.split(" ")
+                            # ['1', '4m', '30']
+                            try:
+                                current_result.append(
+                                    [
+                                        int(result_split_split[1][:-1]) * 1000
+                                        + int(result_split_split[2]) * 10
+                                    ]
+                                )
+                            except ValueError:
+                                pass
+                    else:
+                        print(f"Invalid result: {result}")
+
+                    event_json["results"].append(
+                        {
+                            "id": filename,
+                            "results": current_result,
+                        }
+                    )
+            elif re.match(
+                r"^#[0-9]+ High jump Year ([7-9]|10) (fe)?male$",
+                event_name,
+            ):
+                event_name_split = event_name.split(" ")
+                event_number = event_name_split[0][1:]
+                event_year = event_name_split[4]
+                event_gender = event_name_split[5]
+
+                date_start = datetime.datetime(2023, 9, 2, 9, 0, 0)
+                date_start += datetime.timedelta(minutes=(int(event_number) - 1) * 10)
+                event_json = {
+                    "type": "high_jump",
+                    "name": f"High jump Year {event_year} {event_gender}",
+                    "gender": event_gender,
+                    "ystart": year - int(event_year),
+                    "date": date_start.isoformat(),
+                    "results": [],
+                }
+
+                # iterate through rows B7:F(INFINITY)
+                for row in range(6, sheet.shape[0]):
+                    name = sheet.iloc[row, 2]
+                    name_split = name.split(" ")
+                    result = sheet.iloc[row, 4]
+                    filename = (
+                        "-".join(name_split[0:2]).lower()
+                        + "-"
+                        + str(event_json["ystart"])
+                        # + "*"
+                    )
+                    athlete = simple_tally.lookup_athlete(filename, athletes_directory)
+
+                    if not results_split:
+                        continue
+
+                    current_result = []
+
+                    # 1m 20cm1m 25cm1m 35cm1m 40cm1m 45cm1m 50cm1m 55cm1m 60cm
+                    results_split = result.split("cm")
+                    # ['1m 20', '1m 25', '1m 35', '1m 40', '1m 45', '1m 50', '1m 55', '1m 60', '']
+
+                    for result_split in results_split:
+                        if not result_split:
+                            continue
+                        result_split_split = result_split.split(" ")
+                        # ['1m', '20']
+                        try:
+                            current_result.append(
+                                [
+                                    int(result_split_split[0][:-1]) * 1000
+                                    + int(result_split_split[1]) * 10
+                                ]
+                            )
+                        except ValueError:
+                            pass
+
+                    event_json["results"].append(
+                        {
+                            "id": filename,
+                            "results": current_result,
+                        }
+                    )
             if event_json:
                 # write file to output directory
                 event_file = (
@@ -238,6 +342,10 @@ def import_results(file, athletes_directory, output, year):
                         with open(event_file, "w", encoding="utf-8") as f:
                             yaml.dump(event_json, f, sort_keys=False)
                         break
+            else:
+                print(
+                    f"Unknown event type with {sheet.shape[0]-5} results: {event_name}"
+                )
 
 
 if __name__ == "__main__":
