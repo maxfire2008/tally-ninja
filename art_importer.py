ac795325eccfcef5da586e4e61926438c96dc136
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2023-09-09 17:26:37 +1000

    finish art_importer

--- art_importer.py
+++ art_importer.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 import pathlib
+from pprint import pprint
 import re
 import yaml
 import pandas
@@ -26,7 +27,6 @@ def import_agee_race_timing(input: pathlib.Path, data_folder: pathlib.Path):
     race_name = rows[0]["Race Name"]
     race_start_time = rows[0]["Race Start Date/Time"]
     for row in rows:
-        print(row)
         if race_name != row["Race Name"]:
             raise ValueError(
                 f"race_name {race_name!r} doesn't match {row['Race Name']!r}"
@@ -35,8 +35,8 @@ def import_agee_race_timing(input: pathlib.Path, data_folder: pathlib.Path):
             race_start_time = row["Race Start Date/Time"]
 
         athlete_data = {
-            "name": row["First Name"] + " " + row["Last Name"],
-            "gender": row["Sex"],
+            "name": (row["First Name"] + " " + row["Last Name"]).title(),
+            "gender": row["Sex"].lower(),
             "dob": datetime.datetime.strptime(row["DOB"], "%d/%m/%Y")
             .date()
             .isoformat(),
@@ -61,22 +61,65 @@ def import_agee_race_timing(input: pathlib.Path, data_folder: pathlib.Path):
             with athlete_filename.open("w") as f:
                 yaml.dump(athlete_data, f)
 
-        finish_time = row["Finishing Time (in seconds)"]
+        athlete_result = {}
+
+        if row["Finishing Time (in seconds)"]:
+            athlete_result["finish_time"] = simple_tally.number(
+                row["Finishing Time (in seconds)"]
+            )
 
         if sanitized_name in race_results.values():
             raise ValueError(f"Duplicate athlete name: {sanitized_name}")
         else:
-            race_results[sanitized_name] = finish_time
+            if not row["Finishing Time (in seconds)"]:
+                if row["Finishing Time"] in ["DNS", "DNF"]:
+                    athlete_result[row["Finishing Time"]] = True
+                else:
+                    raise ValueError(f"Missing finish time for {sanitized_name}, {row}")
+            elif row["Laps Remaining"] != "0":
+                raise ValueError(f"Missing finish time for {sanitized_name}, {row}")
 
-    if "Short Course" in race_name:
-        pass
+            race_results[sanitized_name] = athlete_result
+
+        for k, v in athlete_data.items():
+            if not v:
+                print(f"Missing value for {k}, filename {sanitized_name}.yaml")
+
+    if "Duathlon Short Course" in race_name:
+        race_distance = "duathlon_short_course"
+    elif "Duathlon Sprint" in race_name:
+        race_distance = "duathlon_sprint"
+    elif "Duathlon Olympic" in race_name:
+        race_distance = "duathlon_olympic"
+    else:
+        raise ValueError(
+            f"Race name {race_name} does not match any specified distances."
+        )
 
     # create the results file
     results_file_json = {
         "type": "race",
         "name": race_name,
+        "distance": race_distance,
         "date": race_start_time,
+        "results": race_results,
     }
+
+    # write the results file
+    race_name_sanitized = re.sub(r"[^a-zA-Z0-9]", "_", race_name).lower()
+    race_filename = results_folder / f"{race_name_sanitized}.yaml"
+
+    if race_filename.exists():
+        # check if the race data matches
+        with race_filename.open() as f:
+            existing_data = yaml.safe_load(f)
+        if existing_data != results_file_json:
+            raise ValueError(
+                f"Existing race data {existing_data} doesn't match {results_file_json}"
+            )
+    else:
+        with race_filename.open("w") as f:
+            yaml.dump(results_file_json, f)
 
 
 def process_folder(input_folder, data_folder):
