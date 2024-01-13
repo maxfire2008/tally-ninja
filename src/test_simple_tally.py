a1ab1ed0920f531c30545bcccddadc09352034d0
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2024-01-13 16:25:05 +1100

    add a test with AC2023 data

--- src/test_simple_tally.py
+++ src/test_simple_tally.py
@@ -2,6 +2,7 @@ import pathlib
 import raceml
 import simple_tally
 import importlib
+import sys
 
 
 def run_tally(data_folder: pathlib.Path) -> dict:
@@ -13,7 +14,7 @@ def run_tally(data_folder: pathlib.Path) -> dict:
         database_lock.release()
 
 
-def test_simple_tally():
+def test_test2():
     data_folder = pathlib.Path(__file__).parent / "simple_tally_tests" / "test2"
 
     # delete folder data_folder/cache
@@ -23,8 +24,29 @@ def test_simple_tally():
             f.unlink()
         cache_folder.rmdir()
 
+    tb = importlib.import_module("simple_tally_tests.test2_expected").tb
+
     for i in range(5):
-        assert (
-            run_tally(data_folder)
-            == importlib.import_module("simple_tally_tests.test2_expected").tb
-        )
+        assert run_tally(data_folder) == tb
+
+
+def test_confidential():
+    data_folder = (
+        pathlib.Path(__file__).parent.parent.parent
+        / "sport-scorer-sample-data"
+        / "AC2023"
+    )
+
+    # delete folder data_folder/cache
+    cache_folder = data_folder / "cache"
+    if cache_folder.exists():
+        for f in cache_folder.iterdir():
+            f.unlink()
+        cache_folder.rmdir()
+
+    # import ..\..\..\sport-scorer-sample-data\AC2023_expected.py . tb
+    sys.path.append(str(data_folder.parent))
+    tb = importlib.import_module("AC2023_expected").tb
+
+    for i in range(5):
+        assert run_tally(data_folder) == tb
