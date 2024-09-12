95cad7879ad0cd009eb3432f9cbef25140885104
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2024-09-12 11:20:11 +1000

    start refactoring

--- editor/browser_editor.py
+++ editor/browser_editor.py
@@ -24,108 +24,11 @@ def get_config():
 
 @app.route("/")
 def index():
-    filepath = app.config["RACEML_DATABASE"] / "competitions"
-    return flask.render_template(
-        "index.html.j2",
-        files=sorted(
-            [
-                {
-                    "name": str(path.relative_to(filepath)),
-                    "link": flask.url_for(
-                        "competition", filepath=str(path.relative_to(filepath))
-                    ),
-                }
-                for path in filepath.glob("**/*.yaml")
-            ],
-            key=lambda x: x["name"],
-        ),
-    )
+    # list files in app.config["RACEML_DATABASE"]/events
+    events = list((app.config["RACEML_DATABASE"] / "events").iterdir())
+    print(events)
 
-
-@app.route("/new_competition", methods=["POST"])
-def new_competition():
-    filename = flask.request.form.get("filename", None)
-    if filename is None:
-        return "No filename specified", 400
-    competition_type = flask.request.form.get("competition_type", None)
-    if competition_type is None:
-        return "No competition type specified", 400
-    competitor_type = flask.request.form.get("competitor_type", None)
-    if competitor_type is None:
-        return "No competitor type specified", 400
-    filepath = app.config["RACEML_DATABASE"] / "competitions" / (filename + ".yaml")
-    if filepath.exists():
-        return "File already exists", 400
-
-    filepath.parent.mkdir(parents=True, exist_ok=True)
-
-    with open(filepath, "w", encoding="utf-8") as file:
-        file.write(
-            yaml.dump(
-                {
-                    "type": competition_type,
-                    "competitor_type": competitor_type,
-                    "results": {},
-                },
-            )
-        )
-
-    return flask.redirect(flask.url_for("competition", filename=filename + ".yaml"))
-
-
-@app.route("/competition/<path:filepath>")
-def competition(filepath):
-    filepath = app.config["RACEML_DATABASE"] / "competitions" / filepath
-    data = yaml.safe_load(filepath.read_text(encoding="utf-8"))
-    if data["type"] in ["race", "bonus_points", "high_jump"]:
-        if "date" in data and data["date"] is not None:
-            data["date"] = data["date"].isoformat()
-        return flask.render_template(
-            "competition_editor.html.j2",
-            data=data,
-            config=get_config(),
-        )
-    else:
-        return "Competition type not supported", 501
-
-
-@app.route("/competition/<path:filepath>", methods=["PUT"])
-def PUT_competition(filepath):
-    # get JSON data from request
-    data = flask.request.json
-
-    # open the existing file
-    reader = ruamel.yaml.YAML()
-    with open(
-        app.config["RACEML_DATABASE"] / "competitions" / filepath,
-        "r",
-        encoding="utf-8",
-    ) as file:
-        doc = reader.load(file)
-
-    if data.get("date") is not None:
-        data["date"] = datetime.datetime.fromisoformat(data["date"])
-
-    # update the doc to be identical to data
-    update_dictionary(doc, data)
-
-    # write the file
-    with open(
-        app.config["RACEML_DATABASE"] / "competitions" / filepath,
-        "w",
-        encoding="utf-8",
-    ) as file:
-        reader.dump(doc, file)
-
-    return "OK"
-
-
-@app.route("/selection_preview")
-def selection_preview():
-    return flask.render_template(
-        "selection_preview.html.j2",
-        config=get_config(),
-    )
+    return "hello"
 
 
 def update_dictionary(dictionary, new_data):
